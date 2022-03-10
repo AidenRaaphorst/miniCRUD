f819b543c823e91f8aa1663bce234443bb154442
AidenRaaphorst <1197043@student.roc-nijmegen.nl>
2022-03-10 10:38:57 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<script src="https://kit.fontawesome.com/02358b2fc2.js" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="css/main.css" />
+		<title>New York Pizza</title>
+	</head>
+	<body>
+		<header>
+			<h1>New York Pizza</h1>
+			<div class="account-details">
+				<h3 class="account-name"></h3>
+				<a href="" title="Account"><i class="fa-solid fa-user"></i></a>
+			</div>
+			<div class="account-box">
+				<h2 id="logged-in">Not logged in</h2>
+				<!-- <form action="/action_page.php">
+					<label for="fname">Email:</label><br />
+					<input type="text" id="fname" name="fname" value="" /><br />
+					<label for="lname">Password:</label><br />
+					<input type="text" id="lname" name="lname" value="" /><br /><br />
+					<input type="" value="Login" />
+					<input type="submit" value="Register" />
+				</form> -->
+				<div class="buttons">
+					<a href="login.php"><button class="account-button">Login</button></a>
+					<a href="register.php"><button class="account-button">Register</button></a>
+				</div>
+			</div>
+		</header>
+
+		<!-- Main -->
+		<main></main>
+
+		<!-- Footer -->
+		<footer></footer>
+	</body>
+</html>
